0f7a16fffe57e4f0b144d06dd44bd1e1682219d0
Johaned <johan.tique@codescrum.com>
2013-12-08 15:11:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailer::Application.config.secret_token = '841788abd2114c273b62f74d9780604080b159fe950d7d1e67e232049ca752de3b1fcc39e6dc289d386cab7f8617954235236034932547b907c94e5e71a58b59'
