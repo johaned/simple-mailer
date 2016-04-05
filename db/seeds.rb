710a7f75e1dfd278db2dbd78b01cd31ed1b31acc
Johan Tique <johan.tique@analisistem.com>
2016-04-04 22:40:28 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,11 @@
 require 'csv'
 
 CSV.foreach(File.join(Rails.root, 'people.csv'), headers: true) do |row|
-  Person.create name: row['Name'].downcase, email: row['Email'].downcase, gender: row['Gender']
+  Person.find_or_create_by name: row['Name'].downcase, email: row['Email'].downcase, gender: row['Gender']
 end
 
-Person.create name: 'Johan Tique', email: 'johan.tique@analisistem.com', gender: 'm', rol: 'admin'
+Person.find_or_create_by name: 'Johan Tique', email: 'johan.tique@gmail.com', gender: 'm', rol: 'assistant'
+Person.find_or_create_by name: 'Johan Tique', email: 'johan.tique@analisistem.com', gender: 'm', rol: 'admin'
+Person.find_or_create_by name: 'Johan Tique', email: 'johan_tique@yahoo.com.co', gender: 'm', rol: 'admin'
+Person.find_or_create_by name: 'Johan Tique', email: 'johan_tique@hotmail.com', gender: 'm', rol: 'admin'
+Person.find_or_create_by name: 'Felipe Muñoz', email: 'pipebermeo14@gmail.com', gender: 'm', rol: 'admin'
