2c080971596459cffa3ed6f6c9ff7640121ef027
Johan Tique <johan.tique@analisistem.com>
2016-09-09 00:22:01 -0500

    move the heroku deploy ment hack into boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,17 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+###### Heroku deployment hack ######
+# copy mongoid config example file into 'real' file
+if ENV['RACK_ENV'] == 'production'
+  # Mongo
+  config_root = File.join(Bundler.root, 'config')
+  mongoid_example_path = File.join(config_root, 'mongoid.yml.example')
+  mongoid_real_path = File.join(config_root, 'mongoid.yml')
+  `cp #{mongoid_example_path} #{mongoid_real_path}`
+  # Email provider
+  email_provider_example_path = File.join(config_root, 'email_provider.yml.example')
+  email_provider_real_path = File.join(config_root, 'email_provider.yml')
+  `cp #{email_provider_example_path} #{email_provider_real_path}`
+end
