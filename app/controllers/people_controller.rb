a53a0e2f8df07ef8d418f062a508568f6ab55130
Johan Tique <johan.tique@analisistem.com>
2016-09-08 23:44:11 -0500

    add strong parameteres to people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -40,7 +40,7 @@ class PeopleController < ApplicationController
   # POST /people
   # POST /people.json
   def create
-    @person = Person.new(params[:person])
+    @person = Person.new person_params
 
     respond_to do |format|
       if @person.save
@@ -59,7 +59,7 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
 
     respond_to do |format|
-      if @person.update_attributes(params[:person])
+      if @person.update_attributes person_params
         format.html { redirect_to @person, notice: 'Person was successfully updated.' }
         format.json { head :no_content }
       else
@@ -79,5 +79,11 @@ class PeopleController < ApplicationController
       format.html { redirect_to people_url }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :rol, :gender, :email)
   end
 end
