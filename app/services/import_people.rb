088623b9a7645db2ca13f06a3a8a515408e5ae9c
Johan Tique <johan.tique@gmail.com>
2016-09-13 15:25:45 -0500

    Improve import_people.rb service object

--- app/services/import_people.rb
+++ app/services/import_people.rb
@@ -9,12 +9,12 @@ class ImportPeople
   #
   # Just a call method
   #
-  # @param [ File ] import_people_form
+  # @param [ File, String ] file_reference, it could be either a file path or a file, it represents a way for getting the csv content
   #
   # @return [ Boolean ]
   #
-  def call(import_people_form)
-    CSV.foreach(import_people_form.file.tempfile, headers: true) do |row|
+  def call(file_reference)
+    CSV.foreach(file_reference, headers: true) do |row|
       Person.find_or_create_by name: row['Name'].to_s.downcase, email: row['Email'].downcase, gender: row['Gender'], rol: row['Role']
     end
     true
